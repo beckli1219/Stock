8f379d8b6fdc0a481d4961e7b65be881e450858f
beckli1219 <beckli1219@gmail.com>
2021-11-23 21:57:37 -0500

    Create test_board.py

---
+++ test_board.py
@@ -0,0 +1,34 @@
+def test_mark_o():
+    board = Board()
+    board.mark_square([0, 0])
+    assert board.squares == [
+        ["O", None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
+
+def test_mark_o_then_x():
+    board = Board()
+    board.mark_square([0,0])
+    board.mark_square([1,1])
+    assert board.squares == [
+        ["O", None, None],
+        [None, "X", None],
+        [None, None, None],
+    ]
+
+def test_empty_board():
+    board = Board()
+    assert board.squares == [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
+
+
+import pytest
+def test_cannot_mark_a_square_twice():
+    board = Board()
+    board.mark_square([1,1])
+    with pytest.raises(ValueError):  
+        board.mark_square([1,1])
